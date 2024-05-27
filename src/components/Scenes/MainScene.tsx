2c4d6c6121216cf6d4dd438d62c5b5e4bffc9e53
Andy <onsubmit@outlook.com>
2024-05-27 13:09:05 -0700

    Add n-gon

--- src/components/Scenes/MainScene.tsx
+++ src/components/Scenes/MainScene.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from 'react';
 
 import { CanvasModel } from '../../CanvasModel';
 import { CartesianPlane } from '../../CartesianPlane';
+import { Constants } from '../../Constants';
+import { LineSegment } from '../../Drawables/LineSegment';
 import { Point } from '../../Drawables/Point';
 import { Polygon } from '../../Drawables/Polygon';
 import { Scene, SceneCanvases, SceneProps } from './Scene';
@@ -29,12 +31,12 @@ function MainScene() {
 
     const cartesianPlane: CartesianPlane = {
       x: {
-        min: -6,
-        max: 6,
+        min: -10,
+        max: 10,
       },
       y: {
-        min: -6,
-        max: 6,
+        min: -10,
+        max: 10,
       },
     };
 
@@ -52,21 +54,39 @@ function MainScene() {
         return null;
       }
 
-      const diamond = new Polygon({
-        points: [
-          new Point({ coordinate: { x: -5, y: 0 } }),
-          new Point({ coordinate: { x: 0, y: 5 } }),
-          new Point({ coordinate: { x: 5, y: 0 } }),
-          new Point({ coordinate: { x: 0, y: -5 } }),
-        ],
+      const ngonPoints: Array<Point> = [];
+      const ngonSides = 5;
+      const radius = 10;
+      const twoPiDivSides = Constants.TWO_PI / ngonSides;
+      for (let i = 0; i < ngonSides; i++) {
+        ngonPoints.push(
+          new Point({
+            coordinate: { x: radius * Math.cos(twoPiDivSides * i), y: radius * Math.sin(twoPiDivSides * i) },
+          })
+        );
+      }
+
+      const ngon = new Polygon({
+        points: ngonPoints,
       });
 
-      const splitPoints = diamond.lineSegments.flatMap((segment) => segment.split(10));
+      const splitPoints = ngon.lineSegments.map((segment) => segment.split(75));
+      const splitPointsLength = splitPoints.length;
+      const curveSegments: Array<LineSegment> = [];
+      for (let i = 0; i < splitPointsLength; i++) {
+        const points1 = splitPoints[i]!;
+        const points2 = splitPoints[(i + 1) % splitPointsLength]!;
+        for (let j = 0; j < points1.length; j++) {
+          const point1 = points1[j]!;
+          const point2 = points2[j]!;
+          curveSegments.push(new LineSegment({ point1, point2 }));
+        }
+      }
 
       return {
         background: {
           canvasModel: prev.background?.canvasModel ?? null,
-          drawables: [diamond, ...splitPoints],
+          drawables: [ngon, ...curveSegments],
         },
       };
     });
