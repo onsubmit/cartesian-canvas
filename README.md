7108231739becc8e7a9ceea662d6a102f22001ad
Andy <onsubmit@outlook.com>
2024-05-26 17:58:05 -0700

    Add boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+# cartesian-canvas | onsubmi
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.module.css';
+
+function App() {
+  return <div className={styles.app}>Hello world.</div>;
+}
+
+export default App;
